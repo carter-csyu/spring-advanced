6fb6403ad643f6deb1f738d6700ff005cda325ee
chunsang.yu <chunsang.yu@gmail.com>
2021-11-08 23:23:49 +0900

    feat: add v0 order api

---
+++ src/main/java/com/carter/spring/advanced/app/v0/OrderServiceV0.java
@@ -0,0 +1,17 @@
+package com.carter.spring.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+
+    private final OrderRepositoryV0 orderRepository;
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
